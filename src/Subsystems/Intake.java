b6e46b27db667fea55cfc494fdb6e51539ac4339
Team1323 <madtownrobotics@maderausd.org>
2017-07-14 18:31:16 -0700

    moved pidgeon functions to Pidgeon class

--- src/Subsystems/Intake.java
+++ src/Subsystems/Intake.java
@@ -16,13 +16,11 @@ public class Intake extends Subsystem{
 	public CANTalon getTalon(){
 		return intakeMotor;
 	}
-    public Pidgeon pidgey;
 	public Intake(){
 		intakeMotor = new CANTalon(Ports.INTAKE_MOTOR);
 		intakeMotor.changeControlMode(TalonControlMode.PercentVbus);
 		intakeMotor.reverseOutput(false);
 		intakeMotor.setVoltageRampRate(24);
-		pidgey = new Pidgeon(intakeMotor);
 	}
 	public static Intake getInstance(){
 		return instance;
@@ -34,39 +32,17 @@ public class Intake extends Subsystem{
 	public void intakeReverse(){
 		intakeMotor.set(0.7);
 	}
-	
-	private final Loop pidgeonLoop = new Loop(){
-		@Override
-		public void onStart(){
-			
-		}
-		@Override
-		public void onLoop(){
-			pidgey.update();
-		}
-		@Override
-		public void onStop(){
-			
-		}
-	};
-	public Loop getPidgeonLoop(){
-		return pidgeonLoop;
-	}
 	@Override
 	public synchronized void stop(){
 		intakeMotor.set(0);
 	}
 	@Override
 	public synchronized void zeroSensors(){
-		pidgey.setAngle(0.0);
+		
 	}
 	@Override
 	public void outputToSmartDashboard(){
 		SmartDashboard.putNumber(" Intake Current ", intakeMotor.getOutputCurrent());
 		SmartDashboard.putNumber("Intake Voltage", intakeMotor.getOutputVoltage());
-		
-		SmartDashboard.putNumber(" Heading Angle ", pidgey.getAngle());
-		SmartDashboard.putNumber(" Pigeon Rate ", pidgey.getAngularRate());
-		SmartDashboard.putBoolean(" Pigeon Good ", pidgey.isGood());
 	}
 }
